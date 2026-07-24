cf791dcf4cfd2202d566cc1e578535699462ed33
Chaitanya Bagade <chaitanyabagade9017@gmail.com>
2026-07-24 18:01:51 +0530

    workdone

---
+++ CodeForce/WordCapatalization.java
@@ -0,0 +1,16 @@
+package CodeForce;
+import java.util.Scanner;
+
+public class WordCapatalization {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String word = sc.next();
+
+        String result = Character.toUpperCase(word.charAt(0)) + word.substring(1);
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
