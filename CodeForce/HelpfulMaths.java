b904e2e5d74cc15084e684382463b8385a6ec166
Chaitanya Bagade <chaitanyabagade9017@gmail.com>
2026-07-25 18:36:59 +0530

    updated

---
+++ CodeForce/HelpfulMaths.java
@@ -0,0 +1,22 @@
+package CodeForce;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class HelpfulMaths {
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        String[] arr = s.split("\\+");
+        Arrays.sort(arr);
+
+        System.out.print(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            System.out.print("+" + arr[i]);
+        }
+
+        sc.close();
+    }
+}
