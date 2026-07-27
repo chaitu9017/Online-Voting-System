ec6cbc5debcc8f7464ca6aeb923d6db313444878
Chaitanya Bagade <chaitanyabagade9017@gmail.com>
2026-07-27 19:26:54 +0530

    workdone

---
+++ CodeForce/StoneOnTable.java
@@ -0,0 +1,23 @@
+package CodeForce;
+import java.util.Scanner;
+
+public class StoneOnTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String s = sc.next();
+
+        int count = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+        sc.close();
+    }
+}
