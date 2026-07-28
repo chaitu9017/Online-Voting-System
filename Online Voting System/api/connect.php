8a89d071c4a65d30b045c33c51f0d2c2d3414ef3
Chaitanya Bagade <chaitanyabagade9017@gmail.com>
2026-07-28 21:39:47 +0530

    updated

---
+++ Online Voting System/api/connect.php
@@ -0,0 +1,12 @@
+<?php
+
+$connect=mysqli_connect("localhost","root","","voting") or die("Connection failed!";
+
+if(!$connect){
+    echo "Connected!";
+}
+else{
+    echo "Not connected!";
+}
+
+?>
