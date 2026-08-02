131ed5efe8a5d00e4043f46791295bcfd82382f5
Chaitanya Bagade <chaitanyabagade9017@gmail.com>
2026-08-02 18:08:47 +0530

    workdone

---
+++ CodeForce/translation.java
@@ -0,0 +1,22 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class translation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        String t = sc.next();
+
+         String reversed = new StringBuilder(s).reverse().toString();
+
+        if (reversed.equals(t)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+        sc.close();
+    }
+}
