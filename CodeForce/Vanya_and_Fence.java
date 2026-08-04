6782ad65b5da63341353047f60a865dfcbabbe9a
Chaitanya Bagade <chaitanyabagade9017@gmail.com>
2026-08-04 15:44:21 +0530

    workdone

---
+++ CodeForce/Vanya_and_Fence.java
@@ -0,0 +1,28 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class Vanya_and_Fence {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int h = sc.nextInt();
+
+        int width = 0;
+
+        for (int i = 0; i < n; i++) {
+            int height = sc.nextInt();
+
+            if (height <= h) {
+                width += 1;
+            } else {
+                width += 2;
+            }
+        }
+
+        System.out.println(width);
+
+        sc.close();
+    }
+}
