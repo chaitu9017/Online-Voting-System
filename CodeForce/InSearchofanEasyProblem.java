764e1524da26117a0fef53f23bacca03aa78fcc9
Chaitanya Bagade <chaitanyabagade9017@gmail.com>
2026-08-06 18:41:39 +0530

    workdone

---
+++ CodeForce/InSearchofanEasyProblem.java
@@ -0,0 +1,19 @@
+package CodeForce;
+import java.util.Scanner;
+
+public class InSearchOfAnEasyProblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            count += sc.nextInt();
+        }
+
+        System.out.println(count > 0 ? "HARD" : "EASY");
+
+        sc.close();
+    }
+}
