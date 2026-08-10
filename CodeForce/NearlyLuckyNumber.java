6afbccc899b1baa744ed41b58e1061f7e683a722
Chaitanya Bagade <chaitanyabagade9017@gmail.com>
2026-08-10 16:26:18 +0530

    workdone

---
+++ CodeForce/NearlyLuckyNumber.java
@@ -0,0 +1,29 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class NearlyLuckyNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String n = sc.next();
+
+        int count = 0;
+
+        for (int i = 0; i < n.length(); i++) {
+            char digit = n.charAt(i);
+
+            if (digit == '4' || digit == '7') {
+                count++;
+            }
+        }
+
+        if (count == 4 || count == 7) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+        sc.close();
+    }
+}
