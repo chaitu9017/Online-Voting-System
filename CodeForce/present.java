779c0442d7538320b1ebf4236424353f53c58751
Chaitanya Bagade <chaitanyabagade9017@gmail.com>
2026-08-12 17:57:08 +0530

    workdone

---
+++ CodeForce/present.java
@@ -0,0 +1,24 @@
+package CodeForce;
+import java.util.Scanner;
+
+public class presents {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] answer = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            int p = sc.nextInt();
+            answer[p] = i;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(answer[i] + " ");
+        }
+
+        sc.close();
+    }
+}
