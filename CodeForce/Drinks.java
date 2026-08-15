e78edadd14f243b5bd872da257b0b154702c216b
Chaitanya Bagade <chaitanyabagade9017@gmail.com>
2026-08-15 18:56:16 +0530

    workdone

---
+++ CodeForce/Drinks.java
@@ -0,0 +1,24 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class Drinks {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        double sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += sc.nextInt();
+        }
+
+        double result = sum / n;
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
