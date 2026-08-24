5f7c7cf90cb413b1150e6bc2d8062750475e8f15
Chaitanya Bagade <chaitanyabagade9017@gmail.com>
2026-08-24 16:52:35 +0530

    workdone

---
+++ CodeForce/Ultra_Fast_Mathematician.java
@@ -0,0 +1,28 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class Ultra_Fast_Mathematician {
+     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < a.length(); i++) {
+
+            if (a.charAt(i) == b.charAt(i)) {
+                result.append('0');
+            } else {
+                result.append('1');
+            }
+        }
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
