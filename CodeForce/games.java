68c7a36fd1238fbd50f8ad4b68bcc8533102ecbc
Chaitanya Bagade <chaitanyabagade9017@gmail.com>
2026-08-25 17:17:46 +0530

    workdone

---
+++ CodeForce/games.java
@@ -0,0 +1,35 @@
+package CodeForce;
+import java.util.Scanner;
+
+public class games {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] home = new int[n];
+        int[] guest = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            home[i] = sc.nextInt();
+            guest[i] = sc.nextInt();
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                // i is host, j is guest
+                if (i != j && home[i] == guest[j]) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+
+        sc.close();
+    }
+}
