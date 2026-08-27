547d1ccd52bafe8cb6be0bb77557bcf44a4f4d16
Chaitanya Bagade <chaitanyabagade9017@gmail.com>
2026-08-27 18:45:29 +0530

    Add Againtwentyfive.java with main method
    Workdone

---
+++ CodeForce/Againtwentyfive.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Againtwentyfive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        System.out.println("25");
+    }
+}
