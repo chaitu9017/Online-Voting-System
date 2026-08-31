e530803401bff45e5679a54ff95d5fe3ec818695
Chaitanya Bagade <chaitanyabagade9017@gmail.com>
2026-08-31 17:05:47 +0530

    workdone

---
+++ CodeForce/againtwentyfive.java
@@ -0,0 +1,13 @@
+package CodeForce;
+
+import java.util.*;
+
+public class againtwentyfive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        System.out.println("25");
+    }
+}
