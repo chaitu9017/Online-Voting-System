64e159d985d9bc57be5796735ed9a0296ae53f7c
Chaitanya Bagade <chaitanyabagade9017@gmail.com>
2026-09-01 19:44:22 +0530

    workdone

---
+++ CodeForce/PoliceRecruits.java
@@ -0,0 +1,31 @@
+package CodeForce;
+
+import java.util.*;
+
+public class PoliceRecruits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int police = 0;
+        int untreated = 0;
+
+        for (int i = 0; i < n; i++) {
+            int event = sc.nextInt();
+
+            if (event == -1) {
+                // Crime occurs
+                if (police > 0) {
+                    police--;
+                } else {
+                    untreated++;
+                }
+            } else {
+                police += event;
+            }
+        }
+
+        System.out.println(untreated);
+    }
+}
